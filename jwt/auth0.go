1c1e3d66091ff171552286c91a6faaf982901238
Xander Flood <xander.t.flood@gmail.com>
2020-04-09 20:20:18 -0400

    fix a bunch of comments and error messages

--- jwt/auth0.go
+++ jwt/auth0.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 )
 
-// Tokener is an interface for obtaining anonymous tokens
+// Tokener is an interface for obtaining tokens
 //go:generate counterfeiter . Tokener
 type Tokener interface {
 	GetToken(ctx context.Context) (string, error)
@@ -45,25 +45,25 @@ type tokenResponse struct {
 	AT string `json:"access_token"`
 }
 
-// GetToken returns an anonymous token from the auth0 client
+// GetToken returns an token from the auth0 client
 func (a Auth0Tokener) GetToken(ctx context.Context) (string, error) {
 	req, _ := http.NewRequest("POST", a.url, strings.NewReader(a.payload))
 	req.Header.Add("content-type", "application/json")
 
 	resp, err := a.doer.Do(req)
 	if err != nil {
-		return "", fmt.Errorf("failed obtaining new anonymous access token: %w", err)
+		return "", fmt.Errorf("failed obtaining new access token: %w", err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		return "", fmt.Errorf("failed obtaining new anonymous access token: status code %v", resp.StatusCode)
+		return "", fmt.Errorf("failed obtaining new access token: status code %v", resp.StatusCode)
 	}
 
 	var tr tokenResponse
 	err = json.NewDecoder(resp.Body).Decode(&tr)
 	if err != nil {
-		return "", fmt.Errorf("failed decoding new anonymous access token: %w", err)
+		return "", fmt.Errorf("failed decoding new access token: %w", err)
 	}
 
 	return tr.AT, nil
